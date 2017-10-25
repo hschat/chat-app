16a42db730c2db64e14806b75e633bacb4307c58
ZeroSoulEater <t1@purzner.de>
2017-10-25 17:00:41 +0200

    [MOD] UI Chat

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NativeRouter, Route, Link } from 'react-router-native';
+import { NativeRouter, Router, Switch, Route, Link } from 'react-router-native';
 
 import Chat from './view/chat';
+import EnterName from './view/enterName'
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <NativeRouter>
-        <View>
-          <Route exact path='/'>
-              <Chat/>
-          </Route>
-        </View>
-      </NativeRouter>
+        <NativeRouter>
+            <Switch>
+              <Route exact path='/'>
+                  <EnterName/>
+              </Route>
+              <Route exact path='/chat'>
+                  <Chat/>
+              </Route>
+            </Switch>
+        </NativeRouter>
     );
   }
 }
--- view/chat.js
+++ view/chat.js
@@ -1,36 +1,69 @@
 import React from 'react';
-import { KeyboardAvoidingView, TextInput, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, TextInput, StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import { Icon } from 'react-native-elements'
+import { Link } from 'react-router-native';
+
+import Message from '../components/message'
+
 
 const styles = StyleSheet.create({
     input: {
-        bottom: 0,
-        height: 40,
+        height: 35,
         borderColor: 'gray',
         borderWidth: 1,
-        padding: 10
+        marginLeft: 6,
+        marginRight: 6,
+        marginTop: 2,
+        marginBottom:2,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+     },
+    navBar:{
+        paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+        height:40,
+        backgroundColor: '#d80030'
     },
+    inputWraper:{
+        backgroundColor: '#888888',
+    },
+    end:{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    }
 });
 
 export default class Chat extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { text: '' };
+        this.state = { text: '', messages: [] };
+        //this.setState({messages: {[user: 'timo', msg: 'asdf'], user:'david',}})
     }
     render() {
         return (
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'center',
-            }}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            <View style={{flex: 1}}>
+                <StatusBar barStyle='default' backgroundColor='#d80030' />
+                <View style={styles.navBar}>
+                    <Link to='/'>
+                        <View>
+                        <Icon name='chevron-left' color='#FFFFFF'/>
+                        </View>
+                    </Link>
+                </View>
+                <KeyboardAvoidingView style={styles.end} behavior='padding'>
+
+                    <Message />
+
+                    <View style={styles.inputWraper}>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => this.setState({text})}
+                            value={this.state.text}
+                        />
+                    </View>
+                </KeyboardAvoidingView>
             </View>
         )
     }
 }
-
-
-
