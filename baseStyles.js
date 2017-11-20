a6459f6c4d321663faec9e3cd41d2b47c0431f04
David Schwarzmann <dev@dschwarz.codes>
2017-11-20 18:50:42 +0100

    [MOD] rename folder, add basic style sheet

---
+++ baseStyles.js
@@ -0,0 +1,18 @@
+var React = require('react-native');
+var {StyleSheet, Dimensions, Platform} = React;
+
+module.exports.colors = {
+  primaryColor: 'rgb(216, 0, 48)',
+  primaryColorDark: 'rgb(183, 18, 52)',
+  secondaryColorLight: 'rgb(110, 118, 124)',
+  secondaryColor: 'rgb(75, 86, 94)',
+  secondaryColorDark: 'rgb(68, 74, 78)',
+  accent1Color: 'rgb(215, 216, 216)',
+  accent2Color: 'rgb(234, 235, 235)',
+  accent3Color: 'rgb(255, 255, 255)'
+};
+
+module.exports = StyleSheet.create({
+
+
+});
