14e65f1be72a3e6138512cb7560a1d852c9a0ae3
ZeroSoulEater <t1@purzner.de>
2017-11-20 11:10:14 +0100

    [ADD] header componenent and more checks for regestration

--- view/hello.js
+++ view/hello.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {View, StyleSheet, Image, AsyncStorage} from 'react-native';
 import {Redirect, Link} from 'react-router-native';
 import {
-    FormInput, Button, Header, Spinner, Container, Title, Icon, Left, Body, Right, Input, Form,
+    FormInput, Button, Spinner, Container, Title, Icon, Input, Form,
     Text, Item, Label, Toast
 } from 'native-base'
 
 
 import {Chat} from './chat'
 import LoadingIcon from "../components/loadingIcon";
+import Header from '../components/head'
 import ChatAPI from "../ChatAPI";
 
 const styles = StyleSheet.create({
@@ -44,7 +45,7 @@ export default class Hello extends React.Component {
             password: '',
             loaded: false,
             joined: false,
-            showToast: false
+            showToast: false,
         };
     }
 
@@ -94,14 +95,7 @@ export default class Hello extends React.Component {
                 </Container>
             ) : (
                 <Container>
-                    <Header androidStatusBarColor='#d80030' backgroundColor='#d80030'
-                            style={{height: 75, paddingTop: 20, backgroundColor:'#FFFFFF'}}>
-                        <Left/>
-                        <Body>
-                        <Image source={require('../assets/img/logo-only.png')} style={{height: 40, width: 40}}/>
-                        </Body>
-                        <Right/>
-                    </Header>
+                    <Header/>
                     <Image source={require('../assets/img/bg.png')} style={styles.backgroundImage}>
                         <Form style={styles.middle}>
                             <Item floatingLabel last style={{backgroundColor: 'rgb(255,255,255)'}}>
@@ -126,8 +120,8 @@ export default class Hello extends React.Component {
                                 <Text>Beitreten</Text>
                                 <Icon ios='ios-log-in' android='md-log-in' size={20}/>
                             </Button>
-                            <Link to='auth/signup' style={styles.right}>
-                                <Text style={styles.link}>Account erstellen</Text>
+                            <Link to='auth/signup'  style={styles.right}>
+                                <Text>Account erstellen</Text>
                             </Link>
                         </Form>
                     </Image>
