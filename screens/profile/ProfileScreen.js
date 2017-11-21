660a4d1e7d785c7ea4d26e6cb46f7c707e8e7a1b
ZeroSoulEater <t1@purzner.de>
2017-11-22 00:07:36 +0100

    [ADD] first styles for Profile

--- screens/profile/ProfileScreen.js
+++ screens/profile/ProfileScreen.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from "native-base";
+import {Text, View, Button, H3, Icon, Item, ListItem, Content, Label, Input, Form} from "native-base";
+import {StyleSheet, Image} from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import NavIcons from "../../components/NavIcons";
 import {TouchableOpacity} from "react-native";
+
+const styles = StyleSheet.create({
+    image: {
+        height: 100,
+        borderRadius: 50,
+        width: 100
+    },
+    header:{
+        fontWeight: 'bold'
+    },
+    subheader:{
+        color: '#999999'
+    }
+});
+
 
 export default class ProfileScreen extends Component {
 
@@ -12,13 +29,40 @@ export default class ProfileScreen extends Component {
     constructor(props) {
         super(props);
         this.store = this.props.screenProps.store;
+        console.log(this.store.user);
     }
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>ProfileScreen</Text>
-            </View>
+
+                <Grid style={{backgroundColor: '#FEFEFE'}}>
+                    <Row size={1} style={{marginTop: 50}}>
+                        <Col size={1}>
+                            <Image style={styles.image} source={{uri: 'http://lorempixel.com/400/400/cats/'}}/>
+                        </Col>
+                        <Col size={2}>
+                            <H3 style={styles.header}>{this.store.user.prename} {this.store.user.lastname}</H3>
+                            <Text style={styles.subheader}>{this.store.user.hsid}</Text>
+                            <Text >
+                                <Icon ios='ios-mail-outline' android='ios-mail-outline'/>
+                                <Text> {this.store.user.email}</Text>
+                            </Text>
+
+
+                        </Col>
+                    </Row>
+                    <Row size={3}>
+                        <Content>
+                            <Form>
+                                <Item floatingLabel>
+                                    <Label>Status ändern</Label>
+                                    <Input />
+                                </Item>
+                            </Form>
+                        </Content>
+                    </Row>
+                </Grid>
+
         );
     }
 }
