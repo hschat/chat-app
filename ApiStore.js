7197cd41c1b92b20638d431d771f4266654e9ec6
ZeroSoulEater <t1@purzner.de>
2017-11-23 15:50:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- ApiStore.js
+++ ApiStore.js
@@ -12,108 +12,108 @@ const API_URL = process.env['CHAT_ENDPOINT'];
 @autobind
 export default class ApiStore {
 
-  @observable isAuthenticated = false;
-  @observable isConnecting = false;
-  @observable user = null;
-  @observable messages = [];
-  @observable hasMoreMessages = false;
-  @observable skip = 0;
+    @observable isAuthenticated = false;
+    @observable isConnecting = false;
+    @observable user = null;
+    @observable messages = [];
+    @observable hasMoreMessages = false;
+    @observable skip = 0;
 
-  constructor() {
-    console.log('API:', API_URL);
-    const options = {transports: ['websocket'], pingTimeout: 3000, pingInterval: 5000};
-    const socket = io(API_URL, options);
+    constructor() {
+        console.log('API:', API_URL);
+        const options = {transports: ['websocket'], pingTimeout: 3000, pingInterval: 5000};
+        const socket = io(API_URL, options);
 
-    this.app = feathers()
-      .configure(socketio(socket))
-      .configure(hooks())
-      .configure(authentication({
-        storage: AsyncStorage // To store our accessToken
-      }));
+        this.app = feathers()
+            .configure(socketio(socket))
+            .configure(hooks())
+            .configure(authentication({
+                storage: AsyncStorage // To store our accessToken
+            }));
 
-    this.connect();
+        this.connect();
 
-    this.app.service('messages').on('created', createdMessage => {
-      this.messages.unshift(this.formatMessage(createdMessage));
-    });
+        this.app.service('messages').on('created', createdMessage => {
+            this.messages.unshift(this.formatMessage(createdMessage));
+        });
 
-    this.app.service('messages').on('removed', removedMessage => {
-      this.deleteMessage(removedMessage);
-    });
+        this.app.service('messages').on('removed', removedMessage => {
+            this.deleteMessage(removedMessage);
+        });
 
-    if (this.app.get('accessToken')) {
-      this.isAuthenticated = this.app.get('accessToken') !== null;
+        if (this.app.get('accessToken')) {
+            this.isAuthenticated = this.app.get('accessToken') !== null;
+        }
     }
-  }
 
-  connect() {
-    this.isConnecting = true;
+    connect() {
+        this.isConnecting = true;
 
-    this.app.io.on('connect', () => {
-      this.isConnecting = false;
+        this.app.io.on('connect', () => {
+            this.isConnecting = false;
 
-      this.authenticate().then(() => {
-        console.log('authenticated after reconnection');
-      }).catch(error => {
-        console.log('error authenticating after reconnection', error);
-      });
-    });
+            this.authenticate().then(() => {
+                console.log('authenticated after reconnection');
+            }).catch(error => {
+                console.log('error authenticating after reconnection', error);
+            });
+        });
 
-    this.app.io.on('disconnect', () => {
-      console.log('disconnected');
-      this.isConnecting = true;
-    });
-  }
+        this.app.io.on('disconnect', () => {
+            console.log('disconnected');
+            this.isConnecting = true;
+        });
+    }
 
-  createAccount(userData) {
-    return this.app.service('users').create(userData).then((result) => {
-      return this.authenticate({email: userData.email, password: userData.password, strategy: 'local'})
-    });
-  }
+    createAccount(userData) {
+        return this.app.service('users').create(userData).then((result) => {
+            return this.authenticate({email: userData.email, password: userData.password, strategy: 'local'})
+        });
+    }
 
-  authenticate(options) {
-    options = options ? options : undefined;
-    return this._authenticate(options).then(user => {
-      console.log('authenticated successfully', user.id, user.email);
-      this.user = user;
-      this.isAuthenticated = true;
-      return Promise.resolve(user);
-    }).catch(error => {
-      console.log('authenticated failed', error.message);
-      console.log(error);
-      return Promise.reject(error);
-    });
-  }
+    authenticate(options) {
+        options = options ? options : undefined;
+        return this._authenticate(options).then(user => {
+            console.log('authenticated successfully', user.id, user.email);
+            this.user = user;
+            this.isAuthenticated = true;
+            return Promise.resolve(user);
+        }).catch(error => {
+            console.log('authenticated failed', error.message);
+            console.log(error);
+            return Promise.reject(error);
+        });
+    }
 
-  _authenticate(payload) {
-    return this.app.authenticate(payload)
-      .then(response => {
-        return this.app.passport.verifyJWT(response.accessToken);
-      })
-      .then(payload => {
-        return this.app.service('users').get(payload.userId);
-      }).catch(e => Promise.reject(e));
-  }
+    _authenticate(payload) {
+        return this.app.authenticate(payload)
+            .then(response => {
+                return this.app.passport.verifyJWT(response.accessToken);
+            })
+            .then(payload => {
+                return this.app.service('users').get(payload.userId);
+            }).catch(e => Promise.reject(e));
+    }
 
-  promptForLogout() {
-    Alert.alert('Sign Out', 'Are you sure you want to sign out?',
-      [
-        {
-          text: 'Cancel', onPress: () => {
-        }, style: 'cancel'
-        },
-        {text: 'Yes', onPress: this.logout, style: 'destructive'},
-      ]
-    );
-  }
+    promptForLogout() {
+        Alert.alert('Sign Out', 'Are you sure you want to sign out?',
+            [
+                {
+                    text: 'Cancel', onPress: () => {
+                }, style: 'cancel'
+                },
+                {text: 'Yes', onPress: this.logout, style: 'destructive'},
+            ]
+        );
+    }
 
-  logout() {
-    this.app.logout();
-    this.skip = 0;
-    this.messages = [];
-    this.user = null;
-    this.isAuthenticated = false;
-  }
+    logout() {
+        this.app.logout();
+        this.skip = 0;
+        this.messages = [];
+        this.user = null;
+        this.isAuthenticated = false;
+    }
 
 
   findUser(partial) {
@@ -153,61 +153,61 @@ export default class ApiStore {
   loadMessages(loadNextPage) {
     let $skip = this.skip;
 
-    const query = {query: {$sort: {createdAt: -1}, $skip}};
+        const query = {query: {$sort: {createdAt: -1}, $skip}};
 
-    return this.app.service('messages').find(query).then(response => {
-      const messages = [];
-      const skip = response.skip + response.limit;
+        return this.app.service('messages').find(query).then(response => {
+            const messages = [];
+            const skip = response.skip + response.limit;
 
-      for (let message of response.data) {
-        messages.push(this.formatMessage(message));
-      }
+            for (let message of response.data) {
+                messages.push(this.formatMessage(message));
+            }
 
-      console.log('loaded messages from server', JSON.stringify(messages, null, 2));
-      if (!loadNextPage) {
+            console.log('loaded messages from server', JSON.stringify(messages, null, 2));
+            if (!loadNextPage) {
+                this.messages = messages;
+            } else {
+                this.messages = this.messages.concat(messages);
+            }
+            this.skip = skip;
+            this.hasMoreMessages = response.skip + response.limit < response.total;
+
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    formatMessage(message) {
+        return {
+            _id: message._id,
+            text: message.text,
+            position: message.user._id.toString() === this.user._id.toString() ? 'left' : 'right',
+            createdAt: new Date(message.createdAt),
+            user: {
+                _id: message.user._id ? message.user._id : '',
+                name: message.user.email ? message.user.email : message.name,
+                avatar: message.user.avatar ? message.user.avatar : PLACEHOLDER,
+            }
+        };
+    }
+
+    deleteMessage(messageToRemove) {
+        let messages = this.messages;
+        let idToRemove = messageToRemove.id ? messageToRemove.id : messageToRemove._id;
+
+        messages = messages.filter(function (message) {
+            return message.id !== idToRemove;
+        });
         this.messages = messages;
-      } else {
-        this.messages = this.messages.concat(messages);
-      }
-      this.skip = skip;
-      this.hasMoreMessages = response.skip + response.limit < response.total;
+    }
 
-    }).catch(error => {
-      console.log(error);
-    });
-  }
-
-  formatMessage(message) {
-    return {
-      _id: message._id,
-      text: message.text,
-      position: message.user._id.toString() === this.user._id.toString() ? 'left' : 'right',
-      createdAt: new Date(message.createdAt),
-      user: {
-        _id: message.user._id ? message.user._id : '',
-        name: message.user.email ? message.user.email : message.name,
-        avatar: message.user.avatar ? message.user.avatar : PLACEHOLDER,
-      }
-    };
-  }
-
-  deleteMessage(messageToRemove) {
-    let messages = this.messages;
-    let idToRemove = messageToRemove.id ? messageToRemove.id : messageToRemove._id;
-
-    messages = messages.filter(function (message) {
-      return message.id !== idToRemove;
-    });
-    this.messages = messages;
-  }
-
-  sendMessage(messages = {}, rowID = null) {
-    this.app.service('messages').create({text: messages[0].text}).then(result => {
-      console.log('message created!');
-    }).catch((error) => {
-      console.log('ERROR creating message');
-      console.log(error);
-    });
-  }
+    sendMessage(messages = {}, rowID = null) {
+        this.app.service('messages').create({text: messages[0].text}).then(result => {
+            console.log('message created!');
+        }).catch((error) => {
+            console.log('ERROR creating message');
+            console.log(error);
+        });
+    }
 
 }
--- screens/profile/ProfileScreen.js
+++ screens/profile/ProfileScreen.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
-import {Text, View, Button, H3, Icon, Item, ListItem, Content, Label, Input, Form, List, Body, Left, Right} from "native-base";
+import {
+    Text,
+    View,
+    Button,
+    H3,
+    Icon,
+    Item,
+    ListItem,
+    Content,
+    Label,
+    Input,
+    Form,
+    List,
+    Body,
+    Left,
+    Right
+} from "native-base";
 import {StyleSheet, Image} from 'react-native';
-import { Col, Row, Grid } from 'react-native-easy-grid';
+import {Col, Row, Grid} from 'react-native-easy-grid';
 import NavIcons from "../../components/NavIcons";
 import {TouchableOpacity} from "react-native";
+import TimeAgo from "../../components/TimeAgo";
+
 
 const styles = StyleSheet.create({
     image: {
@@ -11,10 +29,10 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         width: 100
     },
-    header:{
+    header: {
         fontWeight: 'bold'
     },
-    subheader:{
+    subheader: {
         color: '#999999'
     }
 });
@@ -23,7 +41,7 @@ const styles = StyleSheet.create({
 export default class ProfileScreen extends Component {
 
     static navigationOptions = ({navigation, screenProps}) => ({
-        headerRight:(<Button onPress={screenProps.store.promptForLogout} transparent><Text>Abmelden</Text></Button>)
+        headerRight: (<Button onPress={screenProps.store.promptForLogout} transparent><Text>Abmelden</Text></Button>)
     });
 
     constructor(props) {
@@ -32,37 +50,51 @@ export default class ProfileScreen extends Component {
         console.log(this.store.user);
     }
 
+    componentWillMount() {
+        //this.store.
+    }
+
+    renderSettings = () => {
+        return (
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-start'}}>
+                <View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-start'}}>
+                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                        <Icon ios='ios-mail-outline' android='ios-mail-outline'/>
+                        <Text> {this.store.user.email}</Text>
+                    </View>
+                </View>
+                <View>
+                    <Form style={{flex: 1, flexDirection: 'row', alignItems: 'flex-start'}}>
+                        <Item floatingLabel>
+                            <Label>Status ändern</Label>
+                            <Input/>
+                        </Item>
+                    </Form>
+                </View>
+            </View>
+
+
+        )
+    };
+
+
     render() {
         return (
-
-                <Grid style={{backgroundColor: '#FEFEFE'}}>
-                    <Row size={1} style={{marginTop: 50}}>
-                        <Col size={1}>
-                            <Image style={styles.image} source={{uri: 'https://api.adorable.io/avatars/200/' + this.store.user.email + '.png'}}/>
-                        </Col>
-                        <Col size={2}>
-                            <H3 style={styles.header}>{this.store.user.prename} {this.store.user.lastname}</H3>
-                            <Text style={styles.subheader}>{this.store.user.hsid}</Text>
-                            <View style={{flex:1, flexDirection: 'row',alignItems: 'flex-start'}}>
-                                <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                                    <Icon ios='ios-mail-outline' android='ios-mail-outline'/>
-                                    <Text> {this.store.user.email}</Text>
-                                </View>
-                            </View>
-                        </Col>
-                    </Row>
-                    <Row size={3}>
-                        <Content>
-                            <Form>
-                                <Item floatingLabel>
-                                    <Label>Status ändern</Label>
-                                    <Input />
-                                </Item>
-                            </Form>
-                        </Content>
-                    </Row>
-                </Grid>
-
+            <Grid style={{padding: 10}}>
+                <Row size={1} style={{marginTop: 15}}>
+                    <Col size={1}>
+                        <Image style={styles.image} source={{uri: 'http://lorempixel.com/400/400/cats/'}}/>
+                    </Col>
+                    <Col size={2}>
+                        <H3 style={styles.header}>{this.store.user.prename} {this.store.user.lastname}</H3>
+                        <Text style={styles.subheader}>{this.store.user.hsid}</Text>
+                        <TimeAgo time={Date.now()}/>
+                    </Col>
+                </Row>
+                <Row size={3}>
+                    {this.renderSettings()}
+                </Row>
+            </Grid>
         );
     }
 }
