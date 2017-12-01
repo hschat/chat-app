a2bed94f7c1b7a57b6081c49e7c77c21f03b708b
ZeroSoulEater <t1@purzner.de>
2017-12-01 01:55:04 +0100

    [ADD] Blank screen for no error

---
+++ screens/search/InviteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class SearchScreen extends React.Component {
+
+
+    render(){
+        return(
+            <Text>InviteScreen</Text>
+        )
+    }
+}
