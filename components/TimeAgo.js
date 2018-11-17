49cbcaaa299252beba12e35bb7ad5c143cd24ea8
Christian Ziegler <ziegler.christian@kabelmail.de>
2018-11-17 14:52:10 +0100

    use DangerZone of Expo

--- components/TimeAgo.js
+++ components/TimeAgo.js
@@ -27,7 +27,7 @@ export default class TimeAgo extends React.Component {
   }
 
   async componentWillMount() {
-    let locale = await Expo.Localization.getCurrentLocaleAsync();
+    let locale = await Expo.DangerZone.Localization.getCurrentLocaleAsync();
     moment.locale(locale.substring(0,2));
   }
 
