1a6c3f5111c386d798dd01c01f6460ac8ca71168
Christian Minuth <38724813+ChrisMastersoo7@users.noreply.github.com>
2018-12-15 18:19:14 +0100

    Implementation of Groupsearch in Searchscreen. (#70)
    * Implementation of Groupsearch in Searchscreen.
    
    * Fix crash when click on user + fix logo size of group
    
    * add missing editable check

--- ApiStore.js
+++ ApiStore.js
@@ -234,6 +234,42 @@ export default class ApiStore {
         });
     }
 
+    findGroup(partial){
+        partial = `${partial}%`;
+        const query = {
+            query: {
+                type: {
+                    $in: ['group']
+                },
+                is_selfmanaged: true,
+                $or: [
+                    {
+                        name: {
+                            $iLike: partial
+                        }
+                    },
+                    {
+                        description: {
+                            $iLike: partial
+                        }
+                    }
+                ]
+            }
+        };
+
+        return this.app.service('chats').find(query).then(response => {
+            const chats = [];
+            for (let chat in response) {
+                //console.log('#####Chats:',chats);
+                chats.push(response[chat]);
+            }
+            return Promise.resolve(chats);
+        }).catch(error => {
+            console.error('Find Group error: ', error);
+            return Promise.reject(error);
+        });
+    }
+
     /**
      * Returns the a user from the given id
      * @param id of the user you want to find
